a93e7cc3a24e4a1b440e3024888deb0b539c9add
ShyaminiD <shyamini.doddi@orgchats.com>
2026-03-23 21:25:06 +0530

    Added Random module for generating random number

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("GUESSING GAME")
+import random
+
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+inputNum = int(input("Enter a value between 1 and 10 "))
+
+x = range(1,11)
+
+if inputNum in x  and inputNum == random_integer:
+    print(inputNum)
+else:
+    print("invalid Input")
+
+
+
+
---
+++ work_log.md
@@ -0,0 +1,2 @@
+Date: 23-Mar-2026
+1. Added Random module for generating random number
