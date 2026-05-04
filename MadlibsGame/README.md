b125869680b228dd9d09f0ac069d460e10fb9239
ShyaminiD <shyamini0306@gmail.com>
2026-05-04 11:24:39 +0530

    added check for userinput against list of prepositions

---
+++ MadlibsGame/README.md
@@ -0,0 +1,22 @@
+# Madlibs Game
+
+This is a simple game where user fills the  six prepositions to complete a short story
+
+## How it works
+
+* The program has a list prepositios from which the user has to choose. 
+* User is prompted to enter a preposition. 
+* if the entered preposition is in the list, it is accepted and added to userinputs 
+* If not → shows a message and asks again.
+* The user must enter a total of six valid prepositions.
+* At the end, the story is printed using the user's inputs.
+
+## How to run
+
+1. Make sure Python is installed on your system.
+2. Run the file:
+
+
+python main.py
+
+
--- MadlibsGame/main.py
+++ MadlibsGame/main.py
@@ -1,5 +1,8 @@
-print("Madlibs Game")
+print("^^^" * 10)
 
+print("        " "MADLIBS GAME ")
+
+print("^^^" * 10)
 
 sample = """Bunnoo the rabbit planted a tiny seed ___ the garden. 
 'Grow!' said Bunnoo. Nothing happened.
@@ -29,28 +32,33 @@ prepositions = [
     "onto",
     "to",
 ]
- 
-print("clues", prepositions)
-p1 = input("Enter first preposition : ")
-p2 = input("Enter second preposition : ")
-p3 = input("Enter third preposition : ")
-p4 = input("Enter another preposition: ")
-p5 = input("Enter another preposition: ")
-p6 = input("Enter last preposition: ")
 
-story = f"""Bunnoo the rabbit planted a tiny seed {p1} the garden. 
+
+
+user_inputs = []
+
+for i in range(6):
+    while True:
+        p = input(f"Enter a preposistion {i+1} :").strip().lower()
+        if p in prepositions:
+            user_inputs.append(p)
+            print(user_inputs)
+            break
+        else:
+            print("Enter from the list")
+
+
+story = """Bunnoo the rabbit planted a tiny seed {} the garden. 
 'Grow!' said Bunnoo. Nothing happened.
 The next day: "Grow!" Nothing.
-Every day, Bunnoo watered the seed and said hello {p2} the soil.
+Every day, Bunnoo watered the seed and said hello {} the soil.
 One morning, a tiny green shoot appeared. It was so small!
-"It's here!" shouted Bunnoo, jumping {p3} joy.
-The shoot grew and grew {p4} a big, leafy plant. And it had little orange carrots underneath {p5} the soil
-"Good things come {p6} those who wait," said Bunnoo's grandma.
-Bunnoo smiled. He was glad he did not stop trying."""
+"It's here!" shouted Bunnoo, jumping {} joy.
+The shoot grew and grew {} a big, leafy plant. And it had little orange carrots underneath {} the soil
+"Good things come {} those who wait," said Bunnoo's grandma.
+Bunnoo smiled. He was glad he did not stop trying.""".format(*user_inputs)
 
-print("*" * 15,"THE FINAL STORY", "*" * 15)
+print("*" * 15, "THE FINAL STORY", "*" * 15)
 print(story)
 
-print("*" * 15,"THE END", "*" * 15)
-
-
+print("*" * 15, "THE END", "*" * 15)
