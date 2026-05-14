32edaa9b52588fe4d3cca95e8fb580206e05b529
ShyaminiD <shyamini0306@gmail.com>
2026-05-14 12:47:16 +0530

    Weather API Integration

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -1,8 +1,36 @@
 import tkinter as tk
 
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+
+
+def get_Weather(place):
+    try :
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={place}&appid={api_key}")
+        result = response.json()
+        print(result)
+        latitude = result['coord']['lat']
+        longitude = result['coord']['lon']
+        description = result['weather'][0]['description']
+        temp = result['main']['temp']
+        max_temp = result['main']['temp_max']
+        min_temp = result['main']['temp_min']
+        weather_Result.config(text = f"{place} \n Latitude : {latitude}\n Longitute : {longitude} \n{description} \n Temperature : {temp}\n Max Temperature : {max_temp} \n Min Temperature : {min_temp}")
+
+    except  requests.exceptions.RequestException as e:
+        print(e)
+
+
 def get_data():
     place = input_place.get()
-    print(place)
+    get_Weather(place)
+   
+    
     
 root = tk.Tk()
 
@@ -12,11 +40,14 @@ root.title("Weather Application")
 label = tk.Label(root, text="Enter Place")
 label.pack()
 
-input_place = tk.Entry(root, font=("Arial", 15, "bold"))
+input_place = tk.Entry(root, font=("Arial", 12, "bold") )
 input_place.pack()
 
 get_Button = tk.Button(root,text = "Get Weather", command = get_data)
 get_Button.pack()
+
+weather_Result = tk.Label(root, text= "Result is ")
+weather_Result.pack()
 root.mainloop()
 
 
--- WeatherApp/worklog.md
+++ WeatherApp/worklog.md
@@ -1,2 +1,6 @@
 Date: 07-May-2026
 1. Created GUI using Tkinter, added button and input.
+
+Date: 14-May-2026
+1. Integrated OpenWeatherMap Api using 'requests' library and able to display required weather details in GUI. 
+
