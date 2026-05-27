e3d844cf669259336c477b4f6b5663fec8a2adf8
ShyaminiD <shyamini0306@gmail.com>
2026-05-27 22:46:41 +0530

    Integrated Open Weather API

--- WeatherApp-Flask/main.py
+++ WeatherApp-Flask/main.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, request
+import requests
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET", "POST"])
-def HomePage():
+load_dotenv()
 
-    return render_template("index.html")
+api_key = os.getenv("API_KEY")
+
+
+@app.route("/", methods=["GET", "POST"])
+def HomePage():
+    weatherData = None
+    if request.method == "GET":
+        print("GET method")
+        place = request.args.get("place","").strip()
+        print("PLACE", place)
+        if place:
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={place}&appid={api_key}&units=metric"
+            response = requests.get(url)
+            weatherData = response.json()
+            print(weatherData)
+            print("HELLO World")
+
+    return render_template("index.html", place=place, weatherData=weatherData)
 
 
 if __name__ == "__main__":
--- WeatherApp-Flask/worklog.md
+++ WeatherApp-Flask/worklog.md
@@ -3,3 +3,6 @@ Date: 23-May-2026
 
 Date: 24-May-2026
 2. Added getbootstrap form.
+
+Date: 27-May-2026
+3. Integrated OpenWeather API.
