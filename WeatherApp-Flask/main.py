5b483f0d62621b9bd64fc936a6c6cded0174bfac
ShyaminiD <shyamini0306@gmail.com>
2026-06-15 21:08:15 +0530

    Able to fetch data and display in ui, default location set to location fecthed from IP address. Added ui using getbootstrap.

--- WeatherApp-Flask/main.py
+++ WeatherApp-Flask/main.py
@@ -15,7 +15,14 @@ def HomePage():
     weatherData = None
     if request.method == "GET":
         print("GET method")
-        place = request.args.get("place","").strip()
+
+        place = request.args.get("place", "").strip()
+        display_place = place
+        if not place:
+            defaultlocation_response = requests.get("http://ip-api.com/json/").json()
+            print(defaultlocation_response["city"])
+            place = defaultlocation_response["city"]
+
         print("PLACE", place)
         if place:
             url = f"https://api.openweathermap.org/data/2.5/weather?q={place}&appid={api_key}&units=metric"
@@ -23,8 +30,14 @@ def HomePage():
             weatherData = response.json()
             print(weatherData)
             print("HELLO World")
+        if weatherData:
+            display_place = " "
+    return render_template("index.html", place=display_place, weatherData=weatherData)
 
-    return render_template("index.html", place=place, weatherData=weatherData)
+
+@app.route("/about")
+def AboutPage():
+    return "This is the About Page"
 
 
 if __name__ == "__main__":
--- WeatherApp-Flask/worklog.md
+++ WeatherApp-Flask/worklog.md
@@ -6,3 +6,7 @@ Date: 24-May-2026
 
 Date: 27-May-2026
 3. Integrated OpenWeather API.
+
+Date : 15-May-2026
+4. Able to fetch data and display in ui, default location set to location fecthed from IP address.
+5. Added ui using getbootstrap.
